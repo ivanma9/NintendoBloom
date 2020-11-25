d9da9584b0adf5911906c059dba267aa0e456339
watermelondruh <37672683+watermelondruh@users.noreply.github.com>
2020-11-25 15:25:22 -0800

    Add files via upload

--- script.js
+++ script.js
@@ -8,16 +8,27 @@ const getPokemonFromAPI = () => {
 	else {
 		const name = inputVal.value;
 		fetch(`https://pokeapi.co/api/v2/pokemon/${name}/`) 
-		.then( (response) => response.json())
+		.then( (response) => response.json() )
 		.then( (resJson) => {
             console.log(resJson);
             //var obj = JSON.parse(resJson);
-            pokeName.innerHTML = resJson.name + " number: " + resJson.id;
+			pokeName.innerHTML = resJson.name + " number: " + resJson.id;
+			let imgURL = resJson.sprites.front_default;
+			displayImage(imgURL, 90, 90, name);
 		})
 		.catch( (err) => {
 			console.log(err);
 		});
 	}
+}
+
+const displayImage = function (src, width, height, alt) {
+	let img = document.createElement("img");
+	img.src = src;
+    img.width = width;
+    img.height = height;
+	img.alt = alt;
+	document.body.appendChild(img);
 }
 
 pokeSearchBtn.addEventListener("click", getPokemonFromAPI);
