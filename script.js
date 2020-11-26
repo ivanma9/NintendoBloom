b56f68e2fc8fe9c8dda01eb9d1e45211dbf14f97
watermelondruh <37672683+watermelondruh@users.noreply.github.com>
2020-11-25 16:14:37 -0800

    changed to pokeapi.glitch.me api

--- script.js
+++ script.js
@@ -6,18 +6,37 @@ const getPokemonFromAPI = () => {
     if (inputVal.tagName.length == 0)
         alert("Please input a name!");
 	else {
+		
 		const name = inputVal.value;
-		fetch(`https://pokeapi.co/api/v2/pokemon/${name}/`) 
-		.then( (response) => response.json())
+		fetch(`https://cors-anywhere.herokuapp.com/https://pokeapi.glitch.me/v1/pokemon/${name}`) 
+		.then( (response) => response.json() )
 		.then( (resJson) => {
-            console.log(resJson);
+            console.log(resJson[0]);
             //var obj = JSON.parse(resJson);
-            pokeName.innerHTML = resJson.name + " number: " + resJson.id;
+			pokeName.innerHTML = resJson[0].name + " number: " + resJson[0].number;
+			let imgURL = resJson[0].sprite;
+			console.log(imgURL);
+			
+			displayImage(imgURL, 200, 200, name);
 		})
 		.catch( (err) => {
 			console.log(err);
 		});
 	}
+}
+
+function displayImage(src, width, height, alt) {
+	let img = document.createElement("img");
+	img.src = src;
+    img.width = width;
+    img.height = height;
+	img.alt = alt;
+	document.body.removeChild(document.body.lastChild);
+	document.body.appendChild(img);
+}
+
+function removeLastSearchImage() {
+	document.body.removeChild(document.lastChild);
 }
 
 pokeSearchBtn.addEventListener("click", getPokemonFromAPI);
